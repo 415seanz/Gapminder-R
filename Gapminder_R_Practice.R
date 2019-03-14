14d76bea6fd5b0447512d762a5218caa4751d11b
Sean Reavey <415seanz@gmail.com>
2019-03-13 19:37:21 -0700

    Added comment on stacked density plot

--- Gapminder_R_Practice.R
+++ Gapminder_R_Practice.R
@@ -33,4 +33,12 @@ daydollars %>% ggplot() + geom_density(aes(dollars_per_day))+scale_x_continuous(
 gapminder %>% filter(continent=="Africa" & year %in% c("1970","2010") & !is.na(gdp)) %>%
   mutate(dollars_per_day=gdp/population/365) %>% ggplot() + geom_density(aes(dollars_per_day)) + scale_x_continuous(trans="log2") + facet_grid(vars(year))
 
-gapminder %>% filter(continent=="Africa" & year %in% c("1970", "2010") & !is.na(gdp)) %>% mutate(dollars_per_day=gdp/population/365) %>% ggplot() + geom_density(aes(dollars_per_day, fill=region), bw=0.5, position="stack") + facet_grid(vars(year)) + scale_x_continuous(trans="log2")
+#Create stacked density plot showing the density by African region of countries by the number 
+#of GDP dollars per resident per day in 1970 vs 2010. Shows most regions improving conditions 
+#for their residents over this time period, creating the 2nd bump in the chart in 2010
+gapminder %>% filter(continent=="Africa" & year %in% c("1970", "2010") & !is.na(gdp)) %>% 
+  mutate(dollars_per_day=gdp/population/365) %>% 
+  ggplot() 
+  + geom_density(aes(dollars_per_day, fill=region), bw=0.5, position="stack") 
+  + facet_grid(vars(year)) 
+  + scale_x_continuous(trans="log2")
